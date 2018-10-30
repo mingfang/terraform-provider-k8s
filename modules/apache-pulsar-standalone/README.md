27fc755d87bf8985b120ad4b0c8aa523acf7534e
Ming Fang <mingfang@mac.com>
2018-10-30 07:24:30 -0400

    add module apache-pulser-standalone

---
+++ modules/apache-pulsar-standalone/README.md
@@ -0,0 +1,9 @@
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| name | - | string | `apache-pulsar` | no |
+| namespace | - | string | `default` | no |
+| replicas | - | string | `1` | no |
+| image | - | string | `apachepulsar/pulsar-all:2.2.0` | no |
+
---
+++ modules/apache-pulsar-standalone/main.tf
@@ -0,0 +1,91 @@
+variable "name" {
+  default = "apache-pulsar"
+}
+
+variable "namespace" {
+  default = "default"
+}
+
+variable "replicas" {
+  default = 1
+}
+
+variable image {
+  default = "apachepulsar/pulsar-all:2.2.0"
+}
+
+locals {
+  labels = {
+    app     = "${var.name}"
+    name    = "${var.name}"
+    service = "${var.name}"
+  }
+}
+
+resource "k8s_core_v1_service" "apache-pulsar" {
+  metadata {
+    name      = "${var.name}"
+    namespace = "${var.namespace}"
+    labels    = "${local.labels}"
+  }
+
+  spec {
+    ports = [
+      {
+        name = "pulsar"
+        port = 6650
+      },
+      {
+        name = "http-admin"
+        port = 8080
+      },
+    ]
+
+    selector = "${local.labels}"
+  }
+}
+
+resource "k8s_apps_v1_deployment" "apache-pulsar" {
+  metadata {
+    name      = "${var.name}"
+    namespace = "${var.namespace}"
+    labels    = "${local.labels}"
+  }
+
+  spec {
+    replicas = "${var.replicas}"
+
+    selector {
+      match_labels = "${local.labels}"
+    }
+
+    strategy {
+      type = "RollingUpdate"
+
+      rolling_update {
+        max_surge       = "25%"
+        max_unavailable = "25%"
+      }
+    }
+
+    template {
+      metadata {
+        labels = "${local.labels}"
+      }
+
+      spec {
+        containers = [
+          {
+            name  = "pulsar"
+            image = "${var.image}"
+
+            command = [
+              "bin/pulsar",
+              "standalone",
+            ]
+          },
+        ]
+      }
+    }
+  }
+}
