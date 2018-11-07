85071f54e7b1dacf455efdc28b5fcc865faf3c97
Ming Fang <mingfang@mac.com>
2018-11-07 12:37:13 -0500

    add module job

---
+++ modules/kubernetes/job/README.md
@@ -0,0 +1,10 @@
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| command | - | string | - | yes |
+| name | - | string | - | yes |
+| backoff\_limit | - | string | `4` | no |
+| image | - | string | `registry.rebelsoft.com/base` | no |
+| restart\_policy | - | string | `Never` | no |
+
---
+++ modules/kubernetes/job/main.tf
@@ -0,0 +1,37 @@
+variable "name" {}
+
+variable "command" {}
+
+variable image {
+  default = "registry.rebelsoft.com/base"
+}
+
+variable restart_policy {
+  default = "Never"
+}
+
+variable backoff_limit {
+  default = 4
+}
+
+resource "k8s_batch_v1_job" "this" {
+  metadata {
+    name = "${var.name}"
+  }
+
+  spec {
+    template {
+      spec {
+        containers {
+          name    = "base"
+          image   = "${var.image}"
+          command = ["bash", "-cx", "${var.command}"]
+        }
+
+        restart_policy = "${var.restart_policy}"
+      }
+    }
+
+    backoff_limit = "${var.backoff_limit}"
+  }
+}
