c828ea411015c002767c9f2eeec063051919fc30
Ming Fang <mingfang@mac.com>
2018-11-18 09:58:12 -0500

    fmt

--- examples/nfs-server-empty-dir/service.tf
+++ examples/nfs-server-empty-dir/service.tf
@@ -1,8 +1,8 @@
 resource "k8s_core_v1_service" "this" {
   metadata {
-    name      = "${var.name}"
-    namespace = "${var.namespace}"
-    labels    = "${local.labels}"
+    name        = "${var.name}"
+    namespace   = "${var.namespace}"
+    labels      = "${local.labels}"
     annotations = "${var.annotations}"
   }
 
