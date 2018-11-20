ad3723b57ba8ef9d8ac5db90085773798cd72476
Ming Fang <mingfang@mac.com>
2018-11-20 10:32:03 -0500

    add module kubernetes dashboard

---
+++ modules/kubernetes/dashboard/main.tf
@@ -0,0 +1,106 @@
+/**
+ * Documentation
+ *
+ * terraform-docs --sort-inputs-by-required --with-aggregate-type-defaults md
+ *
+ */
+
+/*
+common variables
+*/
+
+variable "name" {}
+
+variable "namespace" {
+  default = ""
+}
+
+variable "replicas" {
+  default = 1
+}
+
+variable image {
+  default = "gcr.io/google_containers/kubernetes-dashboard-amd64:v1.10.0"
+}
+
+variable port {
+  default = 9090
+}
+
+variable "annotations" {
+  type    = "map"
+  default = {}
+}
+
+variable "node_selector" {
+  type    = "map"
+  default = {}
+}
+
+variable "dns_policy" {
+  default = ""
+}
+
+variable "priority_class_name" {
+  default = ""
+}
+
+variable "restart_policy" {
+  default = ""
+}
+
+variable "scheduler_name" {
+  default = ""
+}
+
+variable "service_account_name" {
+  default = ""
+}
+
+variable "termination_grace_period_seconds" {
+  default = 30
+}
+
+variable "session_affinity" {
+  default = ""
+}
+
+variable "service_type" {
+  default = ""
+}
+
+/*
+service specific variables
+*/
+
+/*
+locals
+*/
+
+locals {
+  labels {
+    app     = "${var.name}"
+    name    = "${var.name}"
+    service = "${var.name}"
+  }
+}
+
+/*
+output
+*/
+
+output "name" {
+  value = "${k8s_core_v1_service.this.metadata.0.name}"
+}
+
+output "port" {
+  value = "${k8s_core_v1_service.this.spec.0.ports.0.port}"
+}
+
+output "cluster_ip" {
+  value = "${k8s_core_v1_service.this.spec.0.cluster_ip}"
+}
+
+output "deployment_uid" {
+  value = "${k8s_apps_v1_deployment.this.metadata.0.uid}"
+}
