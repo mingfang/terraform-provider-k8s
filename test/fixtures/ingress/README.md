9592708899432f9cc78fb51096233980ae78ceb3
Ming Fang <mingfang@mac.com>
2018-12-27 03:07:12 -0500

    add ingress-class support

--- test/fixtures/ingress/README.md
+++ test/fixtures/ingress/README.md
@@ -3,6 +3,8 @@
 | Name | Description | Type | Default | Required |
 |------|-------------|:----:|:-----:|:-----:|
 | name |  | string | n/a | yes |
+| namespace |  | string | n/a | yes |
+| ingress\_class |  | string | `"test"` | no |
 | node\_port\_http |  | string | `"30080"` | no |
 | node\_port\_https |  | string | `"30081"` | no |
 
@@ -10,6 +12,7 @@
 
 | Name | Description |
 |------|-------------|
+| ingress\_class |  |
 | node\_port\_http |  |
 | node\_port\_https |  |
 
--- test/fixtures/ingress/main.tf
+++ test/fixtures/ingress/main.tf
@@ -1,5 +1,11 @@
 variable "name" {}
 
+variable "namespace" {}
+
+variable "ingress_class" {
+  default = "test"
+}
+
 variable "node_port_http" {
   default = "30080"
 }
@@ -11,15 +17,20 @@ variable "node_port_https" {
 module "ingress-controller" {
   source          = "git::https://github.com/mingfang/terraform-provider-k8s.git//modules/kubernetes/ingress-nginx"
   name            = "${var.name}-ingress-controller"
-  namespace       = "default"
+  namespace       = "${var.namespace}"
+  ingress_class   = "${var.ingress_class}"
   node_port_http  = "${var.node_port_http}"
   node_port_https = "${var.node_port_https}"
 }
 
+output "ingress_class" {
+  value = "${module.ingress-controller.ingress_class}"
+}
+
 output "node_port_http" {
-  value = "${var.node_port_http}"
+  value = "${module.ingress-controller.node_port_http}"
 }
 
 output "node_port_https" {
-  value = "${var.node_port_https}"
+  value = "${module.ingress-controller.node_port_https}"
 }
