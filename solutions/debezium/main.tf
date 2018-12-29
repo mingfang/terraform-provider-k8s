1afafd2b2948abcf7e98c67871df6e58bcd9ead4
Ming Fang <mingfang@mac.com>
2018-12-29 09:55:08 -0500

    more outputs

--- solutions/debezium/main.tf
+++ solutions/debezium/main.tf
@@ -88,14 +88,27 @@ module "kafka-connect-sink" {
   bootstrap_servers = "${module.kafka.name}:9092"
 }
 
-output "kafka-bootstrap-servers" {
+output "kafka_bootstrap_servers" {
   value = "${module.kafka.name}:9092"
 }
 
-output "kafka-connect-source" {
+output "kafka_connect_source" {
   value = "${module.kafka-connect-source.name}:8083"
 }
 
-output "kafka-connect-sink" {
+output "kafka_connect_sink" {
   value = "${module.kafka-connect-sink.name}:8083"
 }
+
+output "kafka_topic_ui_name" {
+  value = "${module.kafka-topic-ui.name}"
+}
+
+output "kafka_connect_ui_name" {
+  value = "${module.kafka-connect-ui.name}"
+}
+
+//output "kafka-topic-ui-port" {
+//  value = "${module.kafka-topic-ui.port}"
+//}
+
