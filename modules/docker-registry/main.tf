20dc05dd5702ff14b6446c3bea2a6436de723f99
Ming Fang <mingfang@mac.com>
2019-03-09 19:03:55 -0500

    upgrade to terraform 0.12

--- modules/docker-registry/main.tf
+++ modules/docker-registry/main.tf
@@ -3,128 +3,46 @@
  *
  */
 
-/*
-common variables
-*/
-
-variable "name" {}
-
-variable "namespace" {
-  default = ""
-}
-
-variable "replicas" {
-  default = 1
-}
-
-variable image {
-  default = "registry:2"
-}
-
-variable port {
-  default = 5000
-}
-
-variable "annotations" {
-  type    = "map"
-  default = {}
-}
-
-variable "node_selector" {
-  type    = "map"
-  default = {}
-}
-
-variable "dns_policy" {
-  default = ""
-}
-
-variable "priority_class_name" {
-  default = ""
-}
-
-variable "restart_policy" {
-  default = ""
-}
-
-variable "scheduler_name" {
-  default = ""
-}
-
-variable "service_account_name" {
-  default = ""
-}
-
-variable "termination_grace_period_seconds" {
-  default = 30
-}
-
-variable "session_affinity" {
-  default = ""
-}
-
-variable "service_type" {
-  default = ""
-}
-
-/*
-service specific variables
-*/
-
-/*
-locals
-*/
-
 locals {
-  labels {
-    app     = "${var.name}"
-    name    = "${var.name}"
-    service = "${var.name}"
+  parameters = {
+    name        = var.name
+    namespace   = var.namespace
+    annotations = var.annotations
+    replicas    = var.replicas
+    ports       = var.ports
+    containers = [
+      {
+        name  = "registry"
+        image = var.image
+        env   = var.env
+
+        volume_mounts = [
+          {
+            mount_path = "/var/lib/registry"
+            name       = var.volume_claim_template_name
+            sub_path   = var.name
+          },
+        ]
+      },
+    ]
+    volume_claim_templates = [
+      {
+        name               = var.volume_claim_template_name
+        storage_class_name = var.storage_class_name
+        access_modes       = ["ReadWriteOnce"]
+
+        resources = {
+          requests = {
+            storage = var.storage
+          }
+        }
+      }
+    ]
   }
 }
 
-/*
-output
-*/
 
-output "name" {
-  value = "${k8s_core_v1_service.this.metadata.0.name}"
-}
-
-output "port" {
-  value = "${k8s_core_v1_service.this.spec.0.ports.0.port}"
-}
-
-output "cluster_ip" {
-  value = "${k8s_core_v1_service.this.spec.0.cluster_ip}"
-}
-
-output "statefulset_uid" {
-  value = "${k8s_apps_v1_stateful_set.this.metadata.0.uid}"
-}
-
-/*
-statefulset specific
-*/
-
-variable storage_class_name {}
-
-variable storage {}
-
-variable volume_claim_template_name {
-  default = "pvc"
-}
-
-resource "k8s_policy_v1beta1_pod_disruption_budget" "this" {
-  metadata {
-    name = "${var.name}"
-  }
-
-  spec {
-    max_unavailable = 1
-
-    selector {
-      match_labels = "${local.labels}"
-    }
-  }
+module "statefulset-service" {
+  source     = "git::https://github.com/mingfang/terraform-provider-k8s.git//archetypes/statefulset-service"
+  parameters = merge(local.parameters, var.overrides)
 }
