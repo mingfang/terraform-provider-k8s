49a4230f1428a4293ffca77e242ee53474e8ddea
Ming Fang <mingfang@mac.com>
2019-03-10 18:43:48 -0400

    upgrade to terraform 0.12

--- modules/kafka-connect/README.md
+++ modules/kafka-connect/README.md
@@ -1,19 +1,2 @@
 <img src="diagram.svg"/>To view the full size interactive diagram, append ```?sanitize=true``` to the raw URL.
 
-## Inputs
-
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| bootstrap\_servers |  | string | n/a | yes |
-| name |  | string | n/a | yes |
-| image |  | string | `"debezium/connect"` | no |
-| namespace |  | string | `"default"` | no |
-| node\_selector |  | map | `{}` | no |
-| replicas |  | string | `"1"` | no |
-
-## Outputs
-
-| Name | Description |
-|------|-------------|
-| name |  |
-
--- modules/kafka-connect/main.tf
+++ modules/kafka-connect/main.tf
@@ -1,150 +1,52 @@
-/*
-common variables
-*/
-
-variable "name" {}
-
-variable "namespace" {
-  default = "default"
-}
-
-variable "replicas" {
-  default = 1
-}
-
-variable image {
-  default = "debezium/connect"
-}
-
-variable "node_selector" {
-  type    = "map"
-  default = {}
-}
-
-/*
-service specific variables
-*/
-
-variable bootstrap_servers {}
-
 locals {
-  labels = {
-    app     = "${var.name}"
-    name    = "${var.name}"
-    service = "${var.name}"
-  }
-}
-
-/*
-service
-*/
-
-resource "k8s_core_v1_service" "connect" {
-  metadata {
-    name      = "${var.name}"
-    namespace = "${var.namespace}"
-    labels    = "${local.labels}"
-  }
-
-  spec {
-    selector = "${local.labels}"
-
-    ports = [
+  parameters = {
+    name        = var.name
+    namespace   = var.namespace
+    annotations = var.annotations
+    replicas    = var.replicas
+    ports       = var.ports
+    containers = [
       {
-        name = "tcp1"
-        port = 8083
-      },
-      {
-        name = "tcp2"
-        port = 5005
+        name  = "connect"
+        image = var.image
+        env = concat([
+          {
+            name  = "BOOTSTRAP_SERVERS"
+            value = var.bootstrap_servers
+          },
+          {
+            name  = "GROUP_ID"
+            value = var.name
+          },
+          {
+            name  = "CONFIG_STORAGE_TOPIC"
+            value = "${var.name}_connect_configs"
+          },
+          {
+            name  = "OFFSET_STORAGE_TOPIC"
+            value = "${var.name}_connect_offsets"
+          },
+          {
+            name  = "HOST_NAME"
+            value = "0.0.0.0"
+          },
+        ], var.env)
+
+        liveness_probe = {
+          failure_threshold = 3
+
+          http_get = {
+            path   = "/"
+            port   = var.ports.0.port
+            scheme = "HTTP"
+          }
+        }
       },
     ]
   }
 }
 
-/*
-deployment
-*/
-
-resource "k8s_apps_v1_deployment" "connect" {
-  metadata {
-    name      = "${var.name}"
-    namespace = "${var.namespace}"
-    labels    = "${local.labels}"
-  }
-
-  spec {
-    replicas = "${var.replicas}"
-
-    selector {
-      match_labels = "${local.labels}"
-    }
-
-    strategy {
-      type = "RollingUpdate"
-
-      rolling_update {
-        max_surge       = "25%"
-        max_unavailable = "25%"
-      }
-    }
-
-    template {
-      metadata {
-        labels = "${local.labels}"
-      }
-
-      spec {
-        node_selector = "${var.node_selector}"
-
-        containers = [
-          {
-            name  = "connect"
-            image = "${var.image}"
-
-            env = [
-              {
-                name  = "BOOTSTRAP_SERVERS"
-                value = "${var.bootstrap_servers}"
-              },
-              {
-                name  = "GROUP_ID"
-                value = "${var.name}"
-              },
-              {
-                name  = "CONFIG_STORAGE_TOPIC"
-                value = "${var.name}_connect_configs"
-              },
-              {
-                name  = "OFFSET_STORAGE_TOPIC"
-                value = "${var.name}_connect_offsets"
-              },
-              {
-                name  = "HOST_NAME"
-                value = "0.0.0.0"
-              },
-            ]
-
-            liveness_probe {
-              failure_threshold = 3
-
-              http_get {
-                path   = "/"
-                port   = 8083
-                scheme = "HTTP"
-              }
-            }
-
-            resources {}
-          },
-        ]
-
-        security_context {}
-      }
-    }
-  }
-}
-
-output "name" {
-  value = "${k8s_core_v1_service.connect.metadata.0.name}"
+module "deployment-service" {
+  source     = "git::https://github.com/mingfang/terraform-provider-k8s.git//archetypes/deployment-service"
+  parameters = merge(local.parameters, var.overrides)
 }
