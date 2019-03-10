dacbaad66583d9d931d76bba5c2326dbaab14f3c
Ming Fang <mingfang@mac.com>
2019-03-10 00:53:55 -0500

    upgrade to terraform 0.12

--- solutions/central-logging/README.md
+++ solutions/central-logging/README.md
@@ -1,23 +1,2 @@
 <img src="diagram.svg"/>To view the full size interactive diagram, append ```?sanitize=true``` to the raw URL.
 
-Central Logging Solution using Elasticsearch, Fluentbit, and Kibana
-
-## Inputs
-
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| es\_replicas |  | string | n/a | yes |
-| name |  | string | n/a | yes |
-| storage |  | string | n/a | yes |
-| storage\_class\_name |  | string | n/a | yes |
-| namespace |  | string | `"default"` | no |
-
-## Outputs
-
-| Name | Description |
-|------|-------------|
-| elasticsearch\_name |  |
-| elasticsearch\_port |  |
-| kibana\_name |  |
-| kibana\_port |  |
-
--- solutions/central-logging/main.tf
+++ solutions/central-logging/main.tf
@@ -16,41 +16,41 @@ variable "es_replicas" {}
 module "elasticsearch" {
   source             = "git::https://github.com/mingfang/terraform-provider-k8s.git//modules/elasticsearch"
   name               = "${var.name}-es"
-  namespace          = "${var.namespace}"
+  namespace          = var.namespace
   image              = "docker.elastic.co/elasticsearch/elasticsearch:6.5.0"
-  replicas           = "${var.es_replicas}"
-  storage_class_name = "${var.storage_class_name}"
-  storage            = "${var.storage}"
+  replicas           = var.es_replicas
+  storage_class_name = var.storage_class_name
+  storage            = var.storage
 }
 
 module "fluentbit-es" {
   source                    = "git::https://github.com/mingfang/terraform-provider-k8s.git//modules/fluentbit-es"
   name                      = "${var.name}-fluentbit"
-  namespace                 = "${var.namespace}"
-  fluent_elasticsearch_host = "${module.elasticsearch.name}"
-  fluent_elasticsearch_port = "${module.elasticsearch.port}"
+  namespace                 = var.namespace
+  fluent_elasticsearch_host = module.elasticsearch.name
+  fluent_elasticsearch_port = module.elasticsearch.port
 }
 
 module "kibana" {
   source            = "git::https://github.com/mingfang/terraform-provider-k8s.git//modules/kibana"
   name              = "${var.name}-kibana"
-  namespace         = "${var.namespace}"
+  namespace         = var.namespace
   image             = "registry.rebelsoft.com/kibana"
   elasticsearch_url = "http://${module.elasticsearch.name}:${module.elasticsearch.port}"
 }
 
 output "elasticsearch_name" {
-  value = "${module.elasticsearch.name}"
+  value = module.elasticsearch.name
 }
 
 output "elasticsearch_port" {
-  value = "${module.elasticsearch.port}"
+  value = module.elasticsearch.port
 }
 
 output "kibana_name" {
-  value = "${module.kibana.name}"
+  value = module.kibana.name
 }
 
 output "kibana_port" {
-  value = "${module.kibana.port}"
+  value = module.kibana.port
 }
