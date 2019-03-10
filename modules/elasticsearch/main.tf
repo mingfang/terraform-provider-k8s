07f53f6209f67f96b0a27f2c77760b211a826d01
Ming Fang <mingfang@mac.com>
2019-03-09 19:26:02 -0500

    upgrade to terraform 0.12

--- modules/elasticsearch/main.tf
+++ modules/elasticsearch/main.tf
@@ -1,164 +1,170 @@
 /**
  * [Elasticsearch](https://www.elastic.co/products/elasticsearch)
  *
- * Module usage:
- *
- *     module "elasticsearch" {
- *       source             = "git::https://github.com/mingfang/terraform-provider-k8s.git//modules/elasticsearch"
- *       name               = "test-elasticsearch"
- *       storage_class_name = "test-elasticsearch"
- *       storage            = "100Gi"
- *       replicas           = "${k8s_core_v1_persistent_volume.test-elasticsearch.count}"
- *     }
- *     
- *     resource "k8s_core_v1_persistent_volume" "test-elasticsearch" {
- *       count = 3
- *     
- *       metadata {
- *         name = "pvc-test-elasticsearch-${count.index}"
- *       }
- *     
- *       spec {
- *         storage_class_name               = "test-elasticsearch"
- *         persistent_volume_reclaim_policy = "Retain"
- *     
- *         access_modes = [
- *           "ReadWriteMany",
- *         ]
- *     
- *         capacity {
- *           storage = "100Gi"
- *         }
- *     
- *         cephfs {
- *           user = "admin"
- *     
- *           monitors = [
- *             "192.168.2.89",
- *             "192.168.2.39",
- *           ]
- *     
- *           secret_ref {
- *             name = "ceph-secret"
- *             namespace = "default"
- *           }
- *         }
- *       }
- *     }
  */
 
-/*
-common variables
-*/
-
-variable "name" {}
-
-variable "namespace" {
-  default = ""
-}
-
-variable "replicas" {
-  default = 1
-}
-
-variable image {
-  default = "docker.elastic.co/elasticsearch/elasticsearch:6.5.1"
-}
-
-variable port {
-  default = 9200
-}
-
-variable "annotations" {
-  type    = "map"
-  default = {}
-}
-
-variable "node_selector" {
-  type    = "map"
-  default = {}
-}
-
-variable "dns_policy" {
-  default = ""
-}
-
-variable "priority_class_name" {
-  default = ""
-}
-
-variable "restart_policy" {
-  default = ""
-}
-
-variable "scheduler_name" {
-  default = ""
-}
-
-variable "service_account_name" {
-  default = ""
-}
-
-variable "termination_grace_period_seconds" {
-  default = 30
-}
-
-variable "session_affinity" {
-  default = ""
-}
-
-variable "service_type" {
-  default = ""
-}
-
-/*
-service specific variables
-*/
-
-variable "heap_size" {
-  default = "4g"
-}
-
-/*
-locals
-*/
-
 locals {
-  labels {
-    app     = "${var.name}"
-    name    = "${var.name}"
-    service = "${var.name}"
+  parameters = {
+    name        = var.name
+    namespace   = var.namespace
+    annotations = var.annotations
+    replicas    = var.replicas
+    ports       = var.ports
+    containers = [
+      {
+        name  = "elasticsearch"
+        image = "${var.image}"
+
+        env = concat([
+          {
+            name = "POD_NAME"
+
+            value_from = {
+              field_ref = {
+                field_path = "metadata.name"
+              }
+            }
+          },
+          {
+            name  = "cluster.name"
+            value = "${var.name}"
+          },
+          {
+            name  = "node.name"
+            value = "$(POD_NAME)"
+          },
+          {
+            name  = "discovery.zen.ping.unicast.hosts"
+            value = "${var.name}"
+          },
+          {
+            name  = "ES_JAVA_OPTS"
+            value = "-Xms${var.heap_size} -Xmx${var.heap_size}"
+          },
+          {
+            name  = "path.data"
+            value = "/data/$(POD_NAME)"
+          },
+        ], var.env)
+
+        liveness_probe = {
+          failure_threshold     = 3
+          initial_delay_seconds = 120
+          period_seconds        = 10
+          success_threshold     = 1
+          timeout_seconds       = 1
+
+          http_get = {
+            path   = "/"
+            port   = "${var.ports.0.port}"
+            scheme = "HTTP"
+          }
+        }
+
+        readiness_probe = {
+          failure_threshold     = 3
+          initial_delay_seconds = 30
+          period_seconds        = 10
+          success_threshold     = 1
+          timeout_seconds       = 1
+
+          http_get = {
+            path   = "/"
+            port   = "${var.ports.0.port}"
+            scheme = "HTTP"
+          }
+        }
+
+        resources = {
+          requests = {
+            cpu    = "250m"
+            memory = "4Gi"
+          }
+        }
+
+        volume_mounts = [
+          {
+            name       = "${var.volume_claim_template_name}"
+            mount_path = "/data"
+            sub_path   = "${var.name}"
+          },
+        ]
+      },
+    ]
+    init_containers = [
+      {
+        name              = "fix-the-volume-permission"
+        image             = "busybox"
+        image_pull_policy = "IfNotPresent"
+
+
+        command = [
+          "sh",
+          "-c",
+          "mkdir -p /data/$(POD_NAME) && chown -R 1000:1000 /data/$(POD_NAME)",
+        ]
+
+        security_context = {
+          privileged = true
+        }
+
+        volume_mounts = [
+          {
+            name       = "${var.volume_claim_template_name}"
+            mount_path = "/data"
+            sub_path   = "${var.name}"
+          },
+        ]
+      },
+      {
+        name              = "increase-the-vm-max-map-count"
+        image             = "busybox"
+        image_pull_policy = "IfNotPresent"
+
+        command = [
+          "sysctl",
+          "-w",
+          "vm.max_map_count=262144",
+        ]
+
+        security_context = {
+          privileged = true
+        }
+      },
+      {
+        name              = "increase-the-ulimit"
+        image             = "busybox"
+        image_pull_policy = "IfNotPresent"
+
+        command = [
+          "sh",
+          "-c",
+          "ulimit -n 65536",
+        ]
+
+        security_context = {
+          privileged = true
+        }
+      },
+    ]
+    volume_claim_templates = [
+      {
+        name               = var.volume_claim_template_name
+        storage_class_name = var.storage_class_name
+        access_modes       = ["ReadWriteOnce"]
+
+        resources = {
+          requests = {
+            storage = var.storage
+          }
+        }
+      }
+    ]
   }
 }
 
-/*
-output
-*/
 
-output "name" {
-  value = "${k8s_core_v1_service.this.metadata.0.name}"
-}
-
-output "port" {
-  value = "${k8s_core_v1_service.this.spec.0.ports.0.port}"
-}
-
-output "cluster_ip" {
-  value = "${k8s_core_v1_service.this.spec.0.cluster_ip}"
-}
-
-output "statefulset_uid" {
-  value = "${k8s_apps_v1_stateful_set.this.metadata.0.uid}"
-}
-
-/*
-statefulset specific
-*/
-
-variable storage_class_name {}
-
-variable storage {}
-
-variable volume_claim_template_name {
-  default = "pvc"
+module "statefulset-service" {
+  source     = "git::https://github.com/mingfang/terraform-provider-k8s.git//archetypes/statefulset-service"
+  parameters = merge(local.parameters, var.overrides)
 }
