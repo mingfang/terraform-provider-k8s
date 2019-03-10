13693e49e202731158260a9a9a085d93e69fcfc1
Ming Fang <mingfang@mac.com>
2019-03-10 00:54:31 -0500

    upgrade to terraform 0.12

--- test/solutions/central-logging/README.md
+++ test/solutions/central-logging/README.md
@@ -1,18 +1,2 @@
 <img src="diagram.svg"/>To view the full size interactive diagram, append ```?sanitize=true``` to the raw URL.
 
-GOCACHE=off go test -v test/solutions/central-logging/basic_test.go
-
-## Inputs
-
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| ingress\_host |  | string | `"192.168.2.146"` | no |
-| name |  | string | `"test-central-logging"` | no |
-
-## Outputs
-
-| Name | Description |
-|------|-------------|
-| elasticsearch\_url |  |
-| kibana\_url |  |
-
--- test/solutions/central-logging/main.tf
+++ test/solutions/central-logging/main.tf
@@ -1,5 +1,5 @@
 /**
- * GOCACHE=off go test -v test/solutions/central-logging/basic_test.go
+ * go clean -testcache; go test -v test/solutions/central-logging/basic_test.go
  */
 
 variable "name" {
@@ -12,72 +12,67 @@ variable "ingress_host" {
 
 module "ingress-controller" {
   source    = "../../../test/fixtures/ingress"
-  name      = "${var.name}"
-  namespace = "${k8s_core_v1_namespace.this.metadata.0.name}"
+  name      = var.name
+  namespace = k8s_core_v1_namespace.this.metadata.0.name
 }
 
 module "storage" {
   source    = "../../../test/fixtures/storage"
   name      = "${var.name}-es"
-  namespace = "${k8s_core_v1_namespace.this.metadata.0.name}"
-  count     = 1
+  namespace = k8s_core_v1_namespace.this.metadata.0.name
+  replicas  = 1
 }
 
 module "central_logging" {
   source             = "../../../solutions/central-logging"
-  name               = "${var.name}"
-  namespace          = "${k8s_core_v1_namespace.this.metadata.0.name}"
-  storage_class_name = "${module.storage.storage_class_name}"
-  storage            = "${module.storage.storage}"
-  es_replicas        = "${module.storage.count}"
+  name               = var.name
+  namespace          = k8s_core_v1_namespace.this.metadata.0.name
+  storage_class_name = module.storage.storage_class_name
+  storage            = module.storage.storage
+  es_replicas        = module.storage.replicas
 }
 
-resource "k8s_extensions_v1beta1_ingress" "this" {
-  metadata {
-    name      = "${var.name}"
-    namespace = "${k8s_core_v1_namespace.this.metadata.0.name}"
+module "ingress-rules" {
+  source        = "../../../modules/kubernetes/ingress-rules"
+  name          = var.name
+  namespace     = k8s_core_v1_namespace.this.metadata.0.name
+  ingress_class = module.ingress-controller.ingress_class
 
-    annotations {
-      "kubernetes.io/ingress.class" = "${module.ingress-controller.ingress_class}"
-    }
-  }
+  rules = [
+    {
+      host = "${module.central_logging.elasticsearch_name}.${var.ingress_host}.nip.io"
 
-  spec {
-    rules = [
-      {
-        host = "${module.central_logging.elasticsearch_name}.${var.ingress_host}.nip.io"
+      http = {
+        paths = [
+          {
+            path = "/"
 
-        http {
-          paths = [
-            {
-              path = "/"
+            backend = {
+              service_name = module.central_logging.elasticsearch_name
+              service_port = module.central_logging.elasticsearch_port
+            }
+          },
+        ]
+      }
+    },
+    {
+      host = "${module.central_logging.kibana_name}.${var.ingress_host}.nip.io"
 
-              backend {
-                service_name = "${module.central_logging.elasticsearch_name}"
-                service_port = "${module.central_logging.elasticsearch_port}"
-              }
-            },
-          ]
-        }
-      },
-      {
-        host = "${module.central_logging.kibana_name}.${var.ingress_host}.nip.io"
+      http = {
+        paths = [
+          {
+            path = "/"
 
-        http {
-          paths = [
-            {
-              path = "/"
+            backend = {
+              service_name = module.central_logging.kibana_name
+              service_port = module.central_logging.kibana_port
+            }
+          },
+        ]
+      }
+    },
+  ]
 
-              backend {
-                service_name = "${module.central_logging.kibana_name}"
-                service_port = "${module.central_logging.kibana_port}"
-              }
-            },
-          ]
-        }
-      },
-    ]
-  }
 }
 
 output "elasticsearch_url" {
