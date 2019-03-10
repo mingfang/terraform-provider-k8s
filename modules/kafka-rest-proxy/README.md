076c4bf95ddc953dc7f9f08406256713a6b10685
Ming Fang <mingfang@mac.com>
2019-03-10 18:47:52 -0400

    upgrade to terraform 0.12

--- modules/kafka-rest-proxy/README.md
+++ modules/kafka-rest-proxy/README.md
@@ -1,19 +1,2 @@
 <img src="diagram.svg"/>To view the full size interactive diagram, append ```?sanitize=true``` to the raw URL.
 
-## Inputs
-
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| name |  | string | n/a | yes |
-| zookeeper |  | string | n/a | yes |
-| image |  | string | `"confluentinc/cp-kafka-rest"` | no |
-| namespace |  | string | `"default"` | no |
-| node\_selector |  | map | `{}` | no |
-| replicas |  | string | `"1"` | no |
-
-## Outputs
-
-| Name | Description |
-|------|-------------|
-| name |  |
-
--- modules/kafka-rest-proxy/main.tf
+++ modules/kafka-rest-proxy/main.tf
@@ -1,128 +1,34 @@
-/*
-common variables
-*/
-
-variable "name" {}
-
-variable "namespace" {
-  default = "default"
-}
-
-variable "replicas" {
-  default = 1
-}
-
-variable image {
-  default = "confluentinc/cp-kafka-rest"
-}
-
-variable "node_selector" {
-  type    = "map"
-  default = {}
-}
-
-/*
-service specific variables
-*/
-
-variable zookeeper {}
-
 locals {
-  labels = {
-    app     = "${var.name}"
-    name    = "${var.name}"
-    service = "${var.name}"
-  }
-}
-
-/*
-service
-*/
-
-resource "k8s_core_v1_service" "this" {
-  metadata {
-    name      = "${var.name}"
-    namespace = "${var.namespace}"
-    labels    = "${local.labels}"
-  }
-
-  spec {
-    selector = "${local.labels}"
-
-    ports = [
+  parameters = {
+    name        = var.name
+    namespace   = var.namespace
+    annotations = var.annotations
+    replicas    = var.replicas
+    ports       = var.ports
+    containers = [
       {
-        name = "http"
-        port = 8000
+        name  = "kafka-rest-proxy"
+        image = var.image
+        env = concat([
+          {
+            name  = "KAFKA_REST_ZOOKEEPER_CONNECT"
+            value = var.zookeeper
+          },
+          {
+            name  = "KAFKA_REST_HOST_NAME"
+            value = var.name
+          },
+          {
+            name  = "KAFKA_REST_LISTENERS"
+            value = "http://0.0.0.0:8000"
+          },
+        ], var.env)
       },
     ]
   }
 }
 
-/*
-deployment
-*/
-
-resource "k8s_apps_v1_deployment" "this" {
-  metadata {
-    name      = "${var.name}"
-    namespace = "${var.namespace}"
-    labels    = "${local.labels}"
-  }
-
-  spec {
-    replicas = "${var.replicas}"
-
-    selector {
-      match_labels = "${local.labels}"
-    }
-
-    strategy {
-      type = "RollingUpdate"
-
-      rolling_update {
-        max_surge       = "25%"
-        max_unavailable = "25%"
-      }
-    }
-
-    template {
-      metadata {
-        labels = "${local.labels}"
-      }
-
-      spec {
-        node_selector = "${var.node_selector}"
-
-        containers = [
-          {
-            name  = "kafka-rest-proxy"
-            image = "${var.image}"
-
-            env = [
-              {
-                name  = "KAFKA_REST_ZOOKEEPER_CONNECT"
-                value = "${var.zookeeper}"
-              },
-              {
-                name  = "KAFKA_REST_HOST_NAME"
-                value = "${var.name}"
-              },
-              {
-                name  = "KAFKA_REST_LISTENERS"
-                value = "http://0.0.0.0:8000"
-              },
-            ]
-
-            resources {}
-          },
-        ]
-
-        security_context {}
-      }
-    }
-  }
-}
-
-output "name" {
-  value = "${k8s_core_v1_service.this.metadata.0.name}"
+module "deployment-service" {
+  source     = "git::https://github.com/mingfang/terraform-provider-k8s.git//archetypes/deployment-service"
+  parameters = merge(local.parameters, var.overrides)
 }
