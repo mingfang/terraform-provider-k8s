4252c086566ca571294474e6f72ad4e144e1fa41
Ming Fang <mingfang@mac.com>
2019-04-08 01:21:20 -0400

    upgrade to terraform 0.12

--- modules/kubernetes/storage-cephfs/main.tf
+++ modules/kubernetes/storage-cephfs/main.tf
@@ -5,40 +5,64 @@
  *
  */
 
-variable "name" {}
-variable "count" {}
-variable "storage" {}
+resource "k8s_core_v1_persistent_volume" "this" {
+  count = var.replicas
 
-variable "annotations" {
-  type    = "map"
-  default = {}
+  metadata {
+    name        = "pvc-${var.name}-${count.index}"
+    annotations = var.annotations
+  }
+
+  spec {
+    storage_class_name               = var.name
+    persistent_volume_reclaim_policy = "Retain"
+    access_modes                     = ["ReadWriteOnce"]
+
+    capacity = {
+      storage = var.storage
+    }
+
+    cephfs {
+      user     = "${var.user}"
+      monitors = "${var.monitors}"
+      path     = "${var.path}"
+
+      secret_ref {
+        name      = "${var.secret_name}"
+        namespace = "${var.secret_namespace}"
+      }
+    }
+
+    mount_options = var.mount_options
+  }
+
+  lifecycle {
+    ignore_changes = ["metadata"]
+  }
 }
 
-variable "mount_options" {
-  type    = "list"
-  default = []
-}
+resource "k8s_core_v1_persistent_volume_claim" "this" {
+  count = var.replicas
 
-variable "user" {}
-variable "secret_name" {}
-variable "secret_namespace" {}
+  metadata {
+    name        = element(k8s_core_v1_persistent_volume.this.*.metadata.0.name, count.index)
+    namespace   = var.namespace
+    annotations = merge(var.annotations, map("pv-uid", element(k8s_core_v1_persistent_volume.this.*.metadata.0.uid, count.index)))
+  }
 
-variable "monitors" {
-  type = "list"
-}
+  spec {
+    storage_class_name = element(k8s_core_v1_persistent_volume.this.*.spec.0.storage_class_name, count.index)
+    volume_name        = element(k8s_core_v1_persistent_volume.this.*.metadata.0.name, count.index)
+    access_modes       = ["ReadWriteOnce"]
 
-variable "path" {
-  default = "/"
-}
+    resources {
+      requests = {
+        storage = element(k8s_core_v1_persistent_volume.this.*.spec.0.capacity.storage, count.index)
+      }
+    }
+  }
 
-output storage_class_name {
-  value = "${element(k8s_core_v1_persistent_volume_claim.this.*.spec.0.storage_class_name, 0)}"
-}
-
-output storage {
-  value = "${element(k8s_core_v1_persistent_volume_claim.this.*.spec.0.resources.0.requests.storage, 0)}"
-}
-
-output count {
-  value = "${k8s_core_v1_persistent_volume_claim.this.count}"
+  lifecycle {
+    ignore_changes = [metadata]
+  }
 }
