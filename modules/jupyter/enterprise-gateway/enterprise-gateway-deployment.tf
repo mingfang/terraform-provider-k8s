dec0c13af22039a3d869d7b55ebaac6030065188
Ming Fang <mingfang@mac.com>
2019-04-12 22:26:54 -0400

    add jupyter enterprise gateway module

---
+++ modules/jupyter/enterprise-gateway/enterprise-gateway-deployment.tf
@@ -0,0 +1,76 @@
+resource "k8s_apps_v1beta2_deployment" "enterprise-gateway" {
+  metadata {
+    labels = {
+      "app"              = "enterprise-gateway"
+      "component"        = "enterprise-gateway"
+      "gateway-selector" = "enterprise-gateway"
+    }
+    name      = "enterprise-gateway"
+    namespace = var.namespace
+  }
+  spec {
+    selector {
+      match_labels = {
+        "gateway-selector" = "enterprise-gateway"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          "app"              = "enterprise-gateway"
+          "component"        = "enterprise-gateway"
+          "gateway-selector" = "enterprise-gateway"
+        }
+      }
+      spec {
+
+        containers {
+          args = [
+            "--gateway",
+          ]
+
+          env {
+            name  = "EG_NAMESPACE"
+            value = var.namespace
+          }
+          env {
+            name  = "EG_KERNEL_CLUSTER_ROLE"
+            value = "kernel-controller"
+          }
+          env {
+            name  = "EG_SHARED_NAMESPACE"
+            value = "False"
+          }
+          env {
+            name  = "EG_MIRROR_WORKING_DIRS"
+            value = "False"
+          }
+          env {
+            name  = "EG_CULL_IDLE_TIMEOUT"
+            value = "3600"
+          }
+          env {
+            name  = "EG_LOG_LEVEL"
+            value = "DEBUG"
+          }
+          env {
+            name  = "EG_KERNEL_LAUNCH_TIMEOUT"
+            value = "60"
+          }
+          env {
+            name  = "EG_KERNEL_WHITELIST"
+            value = "['r_kubernetes','python_kubernetes','python_tf_kubernetes','python_tf_gpu_kubernetes','scala_kubernetes','spark_r_kubernetes','spark_python_kubernetes','spark_scala_kubernetes']"
+          }
+          image             = "elyra/enterprise-gateway:dev"
+          image_pull_policy = "IfNotPresent"
+          name              = "enterprise-gateway"
+
+          ports {
+            container_port = 8888
+          }
+        }
+        service_account_name = "enterprise-gateway-sa"
+      }
+    }
+  }
+}
