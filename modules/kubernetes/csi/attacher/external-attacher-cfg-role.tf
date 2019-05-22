c8b01176f111b2ed84648e3f8c77ff2f34dcab14
Ming Fang <mingfang@mac.com>
2019-05-22 14:07:20 -0400

    add container storage interface module

---
+++ modules/kubernetes/csi/attacher/external-attacher-cfg-role.tf
@@ -0,0 +1,39 @@
+resource "k8s_rbac_authorization_k8s_io_v1_role" "external-attacher-cfg" {
+  metadata {
+    name      = "${var.name}-cfg"
+    namespace = var.namespace
+  }
+
+  rules {
+    api_groups = [
+      "",
+    ]
+    resources = [
+      "configmaps",
+    ]
+    verbs = [
+      "get",
+      "watch",
+      "list",
+      "delete",
+      "update",
+      "create",
+    ]
+  }
+  rules {
+    api_groups = [
+      "coordination.k8s.io",
+    ]
+    resources = [
+      "leases",
+    ]
+    verbs = [
+      "get",
+      "watch",
+      "list",
+      "delete",
+      "update",
+      "create",
+    ]
+  }
+}
